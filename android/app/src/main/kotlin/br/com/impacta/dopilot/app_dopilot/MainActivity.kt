0c3137775d016c69e65fad7b7491a4ec45bd642d
Rafael Oliveira <rafa.francisco.o@gmail.com>
2025-07-12 11:41:42 -0300

    feat: add splash screen with app logo and branding

---
+++ android/app/src/main/kotlin/br/com/impacta/dopilot/app_dopilot/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.com.impacta.dopilot.app_dopilot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:app_dopilot/screens/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DoPilot',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF7B2CBF)),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen()
+      },
+    );
+  }
+}
